609bc3e3005c59a61333f0c4c6323f213db5ce02
Leonid <kylev77@list.ru>
2026-02-03 19:25:47 +0200

    complete lab4

--- app/src/main/java/com/kulev/myapp/ContentActivity.kt
+++ app/src/main/java/com/kulev/myapp/ContentActivity.kt
@@ -1,11 +1,27 @@
 package com.kulev.myapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ContentActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content)
+
+        // NavController из nav_host_fragment
+        navController = findNavController(R.id.nav_host_fragment)
+
+        // BottomNavigationView
+        val bottomNav =
+            findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        bottomNav.setupWithNavController(navController)
     }
 }
